78c4021acd4bb1513832a0441231f53ae8b946e5
Shibam Pandit <shibampandit31@gmail.com>
2024-12-21 11:48:51 +0530

    first commit

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {    
+    res.send('Hello World!');
+});
+
+export default app;
---
+++ backend/server.js
@@ -0,0 +1,6 @@
+import http from 'http';
+import app from './app';
+
+const server = http.createServer(app);
+
+server.listen(3000);
